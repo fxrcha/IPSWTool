904bd018c6c1f51b3d504e567f264b84b445e75c
HyunCha <truetype24@gmail.com>
2021-03-13 00:18:24 +0900

    fix: added sections to sidebar

--- ipswtool/ContentView.swift
+++ ipswtool/ContentView.swift
@@ -9,52 +9,57 @@ import SwiftUI
 
 struct DefaultView: View {
     var body: some View {
-        Text("Hello, world!")
-        Text("This is SwiftUI 2.").padding()
+        Text("Welcome to IPSWTool!")
+            .font(.largeTitle)
+            .padding()
+        Text("IPSWTool is a all-in-one tool that can download and extract IPSW firmwares.")
+        Text("You can download any version of IPSW that you want (except Apple Watch)")
+        Text("Also, you can extract IPSW firmwares and IMG4 (IM4P Payload) files.")
+            
+        Text("Click sidebar to start using IPSWTool.")
+            .padding(10)
     }
 }
 
 struct ContentView: View {
+    func toggleSidebar() {
+            NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+    }
+    
     var body: some View {
         NavigationView{
             List {
-                NavigationLink(destination: DefaultView()) {
-                    Label("Introduction", systemImage: "message")
-                }
-                Group{
-                    Spacer()
-                    Text("Downloads")
-                    Group{
-                        NavigationLink(destination: DefaultView()) {
-                            Label("iPhone", systemImage: "iphone")
-                        }
-                        NavigationLink(destination: DefaultView()) {
-                            Label("iPad", systemImage: "ipad")
-                        }
-                        NavigationLink(destination: DefaultView()) {
-                            Label("iPod", systemImage: "ipodtouch")
-                        }
-                        NavigationLink(destination: DefaultView()) {
-                            Label("Mac (Apple Silicon)", systemImage: "desktopcomputer")
-                        }
-                        NavigationLink(destination: DefaultView()) {
-                            Label("HomePods", systemImage: "homepod")
-                        }
-                    }
-                    Spacer()
-                    Text("Extract")
-                    Group{
-                        NavigationLink(destination: DefaultView()) {
-                            Label("IPSW", systemImage: "shippingbox.fill")
-                        }
-                        NavigationLink(destination: DefaultView()) {
-                            Label("IMG4", systemImage: "folder.fill.badge.gear")
-                        }
+                Section(header: Text("Welcome!")){
+                    NavigationLink(destination: DefaultView()) {
+                        Label("Introduction", systemImage: "message")
                     }
                 }
-                Group{
-                    Spacer()
-                    Text("About")
+                Section(header: Text("Downloads")){
+                    NavigationLink(destination: DefaultView()) {
+                        Label("iPhone", systemImage: "iphone")
+                    }
+                    NavigationLink(destination: DefaultView()) {
+                        Label("iPad", systemImage: "ipad")
+                    }
+                    NavigationLink(destination: DefaultView()) {
+                        Label("iPod", systemImage: "ipodtouch")
+                    }
+                    NavigationLink(destination: DefaultView()) {
+                        Label("Mac (Apple Silicon)", systemImage: "desktopcomputer")
+                    }
+                    NavigationLink(destination: DefaultView()) {
+                        Label("HomePods", systemImage: "homepod")
+                    }
+                }
+                Section(header: Text("Extract")){
+                    NavigationLink(destination: DefaultView()) {
+                        Label("IPSW", systemImage: "shippingbox.fill")
+                    }
+                    NavigationLink(destination: DefaultView()) {
+                        Label("IMG4", systemImage: "folder.fill.badge.gear")
+                    }
+                }
+                Section(header: Text("About")){
                     NavigationLink(destination: DefaultView()) {
                         Label("Version", systemImage: "info.circle.fill")
                     }
@@ -66,11 +71,12 @@ struct ContentView: View {
         }
         .listStyle(SidebarListStyle())
         .navigationTitle("IPSWTool")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        .toolbar{
+            ToolbarItem(placement: .navigation){
+                Button(action: toggleSidebar, label: {
+                    Image(systemName: "sidebar.left")
+            })
+        }
+        }
     }
 }
